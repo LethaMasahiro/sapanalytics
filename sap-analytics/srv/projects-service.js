4431e0636a3fbd947c0c1f4c457ba6792be063d4
Kaibo Sha <shakaibo1997@gmail.com>
2022-12-28 09:54:33 +0000

    resolved merge issues

--- sap-analytics/srv/projects-service.js
+++ sap-analytics/srv/projects-service.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const cds = require('@sap/cds')
 const log  = cds.log('projects-service')
 const {Projects} = cds.entities('sapanalytics.db')
@@ -15,21 +15,4 @@ module.exports = (srv) =>{
         log.info('on action Projects/end data:', request.data, 'params:', request.params)
         // await UPDATE(Projects).set('endDate', request.now).where({'ID':request.params[0]})
     })
-
-=======
-const cds = require ('@sap/cds')
-const log = cds.log('projects-service')
-const {Projects} = cds.entities('sepaanalytics.db')
-
-module.exports = async (srv) => {
-
-    srv.on ('hasProjectEnded', (request) => {
-        log.info('on function hasProjectEnded data: ', request.data, 'params: ', request.params)
-        request.reply(true)
-    })
-
-    srv.on('end', 'Projects', (request) => {
-        log.info('on action Projects/end data: ', request.data, 'params: ', request.params)
-    })
->>>>>>> 54faafbf9a560f781871634e8487da2f179ebef0
 }
