d05c0ab33fa3d0df31abfbf3ef33c6b2b5c96819
Kaibo Sha <shakaibo1997@gmail.com>
2023-01-24 19:54:19 +0000

    reedit mta file

---
+++ sap-analytics/app/learner-behavior/dist/ext/controller/ObjectPageController.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/mvc/ControllerExtension"],function(e){"use strict";return e.extend("learnerbehavior.ext.controller.ObjectPageController",{override:{onInit:function(){var e=this.base.getExtensionAPI().getModel()}}})});
---
+++ sap-analytics/app/learner-behavior/dist/ext/controller/UserAnalysisObjectPageController.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/core/mvc/ControllerExtension"],function(e){"use strict";return e.extend("learnerbehavior.ext.controller.UserAnalysisObjectPageController",{override:{onInit:function(){var e=this.base.getExtensionAPI().getModel()}}})});
